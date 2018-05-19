af1e063239c6a6ad2b89e0efb0d34c5b9aa389fe
Guzmán <gtoribio89@gmail.com>
2018-05-19 20:23:02 +0200

    VG-Guzman admin 1.3

--- admin/admin.php
+++ admin/admin.php
@@ -7,10 +7,16 @@
 </head>
 <body>
     <div id="lista-admin">
+        <div id="opciones-admin">
+            <span>Artículos</span>
+            <span>Códigos Promoción</span>
+            <span>Usuarios</span>
+        </div>
         <fieldset>
             <legend>Inventario</legend>
             <table id="articulos-admin">
                 <tr>
+                    <th class="campoArticulo">Imagen</th>
                     <th class="campoArticulo">ID</th>
                     <th class="campoArticulo">Nombre</th>
                     <th class="campoArticulo">Descripción</th>
@@ -19,13 +25,14 @@
                     <th class="campoArticulo">Plataforma</th>
                     <th class="campoArticulo">Género</th>
                     <th class="campoArticulo">YouTube</th>
+                    <th class="campoArticulo">Compras</th>
                     <th class="campoArticulo">Fecha Publicación</th>
                     <th class="campoArticulo" colspan="2">Opciones</th>
                 </tr>
                 <?php
                 for($i=0;$i<5;$i++) {
-                    print('<tr>');
-                    for ($j = 0; $j < 9; $j++) {
+                    print('<tr><td class="campoArticulo imagen"></td>');
+                    for ($j = 0; $j < 10; $j++) {
                         print('<td class="campoArticulo">CeldaTest' . $j . '</td>');
 
                     }
