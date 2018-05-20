743e8323e46b0528da4ae2f0453a4b834531201c
Guzmán <gtoribio89@gmail.com>
2018-05-20 22:10:17 +0200

    VG-admin 2.3 Agregar

--- admin/admin-productos.php
+++ admin/admin-productos.php
@@ -12,11 +12,30 @@
 
     if($accion == "agregar") {
         print('<div id="agregar-admin">');
+        print("<form action=agregar.php method='POST' ENCTYPE='multipart/form-data'/>");
         print('<fieldset>');
         print('<legend>Agregar Producto</legend>');
         print('<table id="producto-admin">');
+        print('<tr><td class="nombreArticulo">Nombre</td>');
+        print('<td class="campoArticulo"><input type="text"/></td></tr>');
+        print('<tr><td class="nombreArticulo">Descripcion</td>');
+        print('<td class="campoArticulo"><textarea rows="4" cols="50">Descripción del producto</textarea></td></tr>');
+        print('<tr><td class="nombreArticulo">Precio</td>');
+        print('<td class="campoArticulo"><input type="text"/></td></tr>');
+        print('<tr><td class="nombreArticulo">Requisitos</td>');
+        print('<td class="campoArticulo"><textarea rows="4" cols="50">Requisitos del producto</textarea></td></tr>');
+        print('<tr><td class="nombreArticulo">Plataforma</td>');
+        print('<td class="campoArticulo"><input type="text"/></td></tr>');
+        print('<tr><td class="nombreArticulo">Genero</td>');
+        print('<td class="campoArticulo"><input type="text"/></td></tr>');
+        print('<tr><td class="nombreArticulo">URL YouTube</td>');
+        print('<td class="campoArticulo"><input type="text"/></td></tr>');
+        print('<tr><td class="nombreArticulo">Fecha Publicación</td>');
+        print('<td class="campoArticulo"><input type="text"/></td></tr>');
         print('</table>');
         print('</fieldset>');
+        print('</form>');
+        print('</div>');
     } elseif($accion == "modificar") {
 
     } elseif($accion == "eliminar") {
