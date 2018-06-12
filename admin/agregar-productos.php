822ba70bd047d7321f90dd529f5179ca994bf2cd
Guzmán <gtoribio89@gmail.com>
2018-06-12 19:30:56 +0200

    VG-Guzman Admin ALPHA
    En espera de revisión de bugs

---
+++ admin/agregar-productos.php
@@ -0,0 +1,41 @@
+<?php
+include "../classes/admin-consultas.php";
+/*session_start();
+
+if(!isset($_SESSION["admin"])) {
+    header("location:index.php");
+}*/
+
+$nombre = $_REQUEST["nombre"];
+$descripcion = $_REQUEST["descripcion"];
+$precioStr = $_REQUEST["precio"];
+$precio = (int)$precioStr;
+$requisitos = $_REQUEST["requisitos"];
+$plataforma = $_REQUEST["plataforma"];
+$genero = $_REQUEST["genero"];
+$youtube = $_REQUEST["youtube"];
+$compras = 0;
+$fechpubli = new DateTime($_REQUEST["fechpubli"]);
+$dateBD = $fechpubli->format('d/m/Y');
+
+$imagen;
+if (is_uploaded_file($_FILES['imagen']['tmp_name'])) {
+    if (!is_dir("img/covers/"))
+        mkdir("img/covers/");
+    $destino = "../img/covers/";
+    $unica = time();
+    $imagen = $destino.$unica.$_FILES['imagen']['name'];
+    $imagenBD = "img/covers/".$unica.$_FILES['imagen']['name'];
+    $archivo = $_FILES['imagen']['name'];
+    if (!is_file($imagen)) {
+        move_uploaded_file($_FILES['imagen']['tmp_name'], $imagen);
+        echo "fichero movido";
+    } else
+        echo "no se ha podido mover el fichero ya existe";
+}
+
+$consulta = new Consultas();
+
+$consulta->agregar($nombre,$descripcion,(int)$precio,$requisitos,$plataforma,$genero,$youtube,$imagenBD,$compras,$dateBD);
+header("location:productos.php");
+?>
---
+++ admin/usuarios.php
@@ -0,0 +1,70 @@
+<!DOCTIPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <title>VillaGaming - Administrador - Inventario</title>
+    <link rel="stylesheet" type="text/css" href="../css/global.css">
+    <link rel="stylesheet" type="text/css" href="../css/admin-list.css">
+    <script src="../js/jquery-3.3.1.min.js" type="text/javascript"></script>
+    <script src="../js/admin-list.js" type="text/javascript"></script>
+</head>
+<body class="admin">
+    <?php
+    include '../classes/admin-consultas.php';
+    ?>
+    <div id="lista-admin">
+        <div id="opciones-admin">
+            <span class="inverse">Artículos</span>
+            <span class="normal">Códigos Promoción</span>
+            <span class="normal">Usuarios</span>
+            <?php
+            print('<a href="admin-productos.php?accion=agregar">
+                <span class="normal">Agregar</span>
+            </a>');
+            ?>
+        </div>
+        <fieldset>
+            <legend>Inventario</legend>
+            <table id="articulos-admin">
+                <tr>
+                    <th class="campoArticulo">Imagen</th>
+                    <th class="campoArticulo">ID</th>
+                    <th class="campoArticulo">Nombre</th>
+                    <th class="campoArticulo">Precio</th>
+                    <th class="campoArticulo">Plataforma</th>
+                    <th class="campoArticulo">Género</th>
+                    <th class="campoArticulo">YouTube</th>
+                    <th class="campoArticulo">Compras</th>
+                    <th class="campoArticulo">Fecha Publicación</th>
+                    <th class="campoArticulo" colspan="2">Opciones</th>
+                </tr>
+                <?php
+                $consulta = new Consultas();
+                $productos = array();
+                $productos = $consulta->mostrarTodos();
+
+                foreach($productos as $producto) {
+                    print('<tr><td class="campoArticulo imagen"><img src="../'.$producto['imagen'].'"></td>');
+                    print('<td class="campoArticulo">'.$producto['idproducto'].'</td>');
+                    print('<td class="campoArticulo">'.$producto['nombre'].'</td>');
+                    /*print('<td class="campoArticulo">'.$producto['descripcion'].'</td>');*/
+                    print('<td class="campoArticulo">'.$producto['precio'].' €</td>');
+                    /*print('<td class="campoArticulo">'.$producto['requisitos'].'</td>');*/
+                    print('<td class="campoArticulo">'.$producto['plataforma'].'</td>');
+                    print('<td class="campoArticulo">'.$producto['genero'].'</td>');
+                    print('<td class="campoArticulo"><a href="'.$producto['youtube'].'" target="_blank"><button type="button">ver video</button></a></td>');
+                    print('<td class="campoArticulo">'.$producto['compras'].'</td>');
+                    print('<td class="campoArticulo">'.$producto['fechpubli'].'</td>');
+                    print('<td class="campoArticulo boton"><a href="admin-productos.php?accion=modificar&idproducto='.$producto['idproducto'].'">
+                            <span>Modificar</span></a></td>');
+                    print('<td class="campoArticulo boton">
+                            <a href="admin-productos.php?accion=eliminar&idproducto='.$producto['idproducto'].'">
+                                <span>Eliminar</span>
+                            </a></td></tr>');
+                }
+                ?>
+            </table>
+        </fieldset>
+    </div>
+</body>
+</html>
